fcff8d4bfe6e5204ff580d8e213e0ea950b9b594
priya-1401 <lakshmipriyadwara1234@gmail.com>
2025-01-31 19:57:56 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,181 @@
+from flask import Flask, request, render_template, jsonify
+import pickle
+import re
+from urllib.parse import urlparse
+
+from tld import get_tld 
+
+app = Flask(__name__)
+
+# Load the trained model
+with open('malicious_url_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Feature Engineering Functions*****
+def having_ip_address(url):
+    match = re.search(
+        '(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.'
+        '([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'
+        '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)', url)
+    if match:
+        return 1
+    else:
+        return 0
+
+def abnormal_url(url):
+    hostname = urlparse(url).hostname
+    hostname = str(hostname)
+    match = re.search(hostname, url)
+    if match:
+        return 1
+    else:
+        return 0
+from googlesearch import search
+def google_index(url):
+    site = search(url, 5)
+    return 1 if site else 0
+
+def count_dot(url):
+    count_dot = url.count('.')
+    return count_dot
+
+def count_www(url):
+    url.count('www')
+    return url.count('www')
+
+def count_atrate(url):
+     
+    return url.count('@')
+
+def no_of_dir(url):
+    urldir = urlparse(url).path
+    return urldir.count('/')
+
+def no_of_embed(url):
+    urldir = urlparse(url).path
+    return urldir.count('//')
+
+def shortening_service(url):
+    match = re.search(r'bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
+                  r'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
+                  r'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
+                  r'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
+                  r'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
+                  r'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
+                  r'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|'
+                  r'tr\.im|link\.zip\.net', url)
+
+    if match:
+        return 1
+    else:
+        return 0
+    
+def count_https(url):
+    return url.count('https')
+
+def count_http(url):
+    return url.count('http')
+
+def count_per(url):
+    return url.count('%')
+
+def count_ques(url):
+    return url.count('?')
+
+def count_hyphen(url):
+    return url.count('-')
+
+def count_equal(url):
+    return url.count('=')
+
+def url_length(url):
+    return len(str(url))
+
+def hostname_length(url):
+    return len(urlparse(url).netloc)
+
+def suspicious_words(url):
+    match = re.search('PayPal|login|signin|bank|account|update|free|lucky|service|bonus|ebayisapi|webscr',
+                      url)
+    if match:
+        return 1
+    else:
+        return 0
+
+def digit_count(url):
+    digits = 0
+    for i in url:
+        if i.isnumeric():
+            digits = digits + 1
+    return digits
+
+def letter_count(url):
+    letters = 0
+    for i in url:
+        if i.isalpha():
+            letters = letters + 1
+    return letters
+
+def fd_length(url):
+    urlpath= urlparse(url).path
+    try:
+        return len(urlpath.split('/')[1])
+    except:
+        return 0
+
+def tld_length(url):
+    try:
+        return len(url)
+    except:
+        return -1
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        url = request.form['url']
+
+        # Feature Extraction
+        features = [
+            having_ip_address(url),
+            abnormal_url(url),                                                         
+            count_dot(url),
+            count_www(url),
+            count_atrate(url),
+            no_of_dir(url),
+            no_of_embed(url),
+
+           shortening_service(url),
+           count_https(url),
+           count_http(url),
+
+            count_per(url),
+            count_ques(url),
+            count_hyphen(url),
+            count_equal(url),
+
+            url_length(url),
+            hostname_length(url),
+            suspicious_words(url),
+            digit_count(url),
+            letter_count(url),
+            fd_length(url),
+            tld_length(url)
+
+        ]
+        features = [features]  # Convert to 2D array for prediction
+
+        # Make Prediction
+        prediction = model.predict(features)
+        result = 'Benign' if prediction[0] == 0 else 'Malicious'
+
+        return jsonify({'url': url, 'prediction': result})
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
